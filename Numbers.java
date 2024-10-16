107e20fd0352c469ee6920528ecc40c864405819
xtianah124 <oludechristianah16@email.com>
2024-10-16 22:13:12 +0100

    Uploading of java files to github

---
+++ Numbers.java
@@ -0,0 +1,26 @@
+ import java.util.Scanner;
+
+    public class Numbers{
+      public static void main(String[] args){
+        
+         Scanner input = new Scanner(System.in);
+            
+          System.out.print("Enter 0 to 10 numbers");
+             int  = input.nextLine();
+     
+           int  =  0 to 10 numbers;
+           System.out.print("The squares are %d%n", squares);
+       
+            int squares = 0 to 10 numbers;
+             System.ot.print("The cubes are %d%n", cubes);
+       
+
+
+
+
+
+
+
+}
+
+}
---
+++ Statistics.java
@@ -0,0 +1,20 @@
+import.java.util.Scanner;
+
+public class Statistics{
+ public static void main(String[] args);
+
+  Scanner input = new Scanner(System.in);
+
+  System.out.print("Enter the estimated number of stones");
+   String enter = input.nextLine();
+
+  
+
+}
+
+
+
+}
+
+
+
