a38f1614f70949cc4cc85393df585e6626f255ac
xtianah124 <oludechristianah16@email.com>
2024-11-20 12:15:12 +0100

    Group work

--- Group work/Shapes.java
+++ Group work/Shapes.java
@@ -1,20 +1,30 @@
-Import java.util.Scanner;
+import java.util.Scanner;
 
 public class Shapes{
 	public static void main(String[] args){
 
 	Scanner input = new Scanner(System.in);
 
-	System.out.print("Enter a number: ");
-	int number = input.nextInt();
+		System.out.print("Enter first length: ");
+		int firstlength = input.nextInt();
 
-	if(number == 3cm, 3cm, 3cm){
-		System.out.print("Isoseles");
-	}
+		System.out.print("Enter second length: ");
+		int secondlength = input.nextInt();
 
+		System.out.print("Enter third length: ");
+		int thirdlength = input.nextInt();
 
-
-
+		if(firstlength == secondlength && secondlength == thirdlength){
+		System.out.print("Equilateral Triangle");
+		}
+	
+		if(firstlength == secondlength && firstlength != thirdlength){
+		System.out.print("Isosceles Triangle");
+		}
+		
+		if(firstlength != secondlength && secondlength != thirdlength){
+		System.out.print("Isosceles Triangle");
+		}
 
 
 
