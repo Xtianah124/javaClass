0ac3b355266bb24b5a7a0da99c731d7e653044db
xtianah124 <oludechristianah16@email.com>
2024-11-26 15:14:09 +0100

    26-10-24

---
+++ Mbti/MbtiPersonalityTest2.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class MbtiPersonalityTest2{
+	public static void main(String[] args){
+
+	Scanner input = new Scanner(System.in);
+
+	String [] extroverted = new String [5];
+	String [] introverted = new String [5];
+	String [] sensing = new String [5];
+	String [] intuitive = new String [5];
+	String [] thinking = new String [5];
+	String [] feeling = new String [5];
+	String [] judging = new String [5];
+	String [] perceptive = new String [5];
+
+	System.out.print("What is your name: ");
+	String name = input.nextLine();
+
+
+	("A. espend energy, enjoy groups", "B. converse energy, enjoy one-on-one");
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
