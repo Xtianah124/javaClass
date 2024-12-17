1fb2f2e0b97fb8fc348646639051a1808437a45c
Xtianah124 <oludechristianah16@gmail.com>
2024-12-17 17:21:42 +0100

    javaClass

--- Mbti/MbtiPersonalityTest3.java
+++ Mbti/MbtiPersonalityTest3.java
@@ -7,18 +7,15 @@ public class MbtiPersonalityTest2{
 	Scanner input = new Scanner(System.in);
 
 	String [] extroverted = new extrovert [5];
-	String [] introverted = new introvert [5];
 	String [] sensing = new sensing [5];
-	String [] intuitive = new intuitive [5];
 	String [] thinking = new thinking [5];
-	String [] feeling = new feeling [5];
 	String [] judging = new judging [5];
-	String [] perceptive = new perceptive [5];
 
 	System.out.print("What is your name: ");
 	String name = input.nextLine();
 
-	{1. "expend energy, enjoy groups", "conserve energy, enjoy one-on-one"};
-	{2. "Intepret literally", "look for meaning and possibilities"};
+	
+	1.A. "expend energy, enjoy groups", B."conserve energy, enjoy one-on-one";
+	2.A. "Intepret literally", B."look for meaning and possibilities";
 	}
 }
